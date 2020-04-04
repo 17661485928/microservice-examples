faa963bdcdbf4a50f22430ac4fecc8af64c881b2
qht <17661485928@163.com>
2020-04-04 18:04:45 +0800

    demo示例生产者项目初始化代码工程提交

---
+++ springboot-producer-demo/src/main/java/com/example/springbootproducerdemo/SpringbootProducerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootproducerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProducerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProducerDemoApplication.class, args);
+    }
+
+}
---
+++ springboot-producer-demo/src/test/java/com/example/springbootproducerdemo/SpringbootProducerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootproducerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProducerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
